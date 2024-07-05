c49b86f69c33c4ccd3291e6ddf3103da16205f95
Vladdracoola <vladislav_pv@mail.ru>
2024-07-05 16:55:43 +0700

    Add files via upload
    My first hometask!

---
+++ main.py
@@ -0,0 +1,8 @@
+# "1st Program"
+print(9**0.5*5)
+# "2d Program"
+print(9.99>9.98 and 1000!=1001)
+# "3d Program"
+print(1234%1000//10, 5678%5000//10)
+# "4th Program"
+print((int)(13.42)==(int)(42.13%42*100) or (int)(13.42%13*100)==(int)(42.13))
