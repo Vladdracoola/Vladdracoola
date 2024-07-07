583c5615336c4d7762d0a209c9fa7062f36ed4a3
Vladdracoola <vladislav_pv@mail.ru>
2024-07-07 20:30:20 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_tasks_completed=12
+Hours_spent=1.5
+Course_name='Python'
+Time_spent_per_1_task=(Hours_spent/Number_of_tasks_completed*60)
+print('Курс', Course_name, 'всего задач:',Number_of_tasks_completed,'затрачено часов:', Hours_spent, 'среднее время выполнения', Time_spent_per_1_task, 'минут' )
