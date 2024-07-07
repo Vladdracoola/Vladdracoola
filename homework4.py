b50a07410d84081d60164d9ab3ad39c5e6af7f21
Vladdracoola <vladislav_pv@mail.ru>
2024-07-07 22:16:50 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string=input('Кто ты, воин? ')
+number=len(my_string)
+print( 'Количество символов: ',number)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
