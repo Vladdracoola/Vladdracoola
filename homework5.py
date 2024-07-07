e6694ae7aad5df463bf464f34fef7f25f4903725
Vladdracoola <vladislav_pv@mail.ru>
2024-07-07 23:53:12 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var=1, 0.5,'lol',True
+print('Immutable tuple:',immutable_var)
+#immutable_var[0]=2 Функция кортежа подразумевает неизменность её элементов!
+mutable_list=['Nice',20,False,6.3]
+mutable_list[0:4]='Dios mio!','Good',True,3.6
+print('Mutable list:',mutable_list)
