b34bd502f515fa4a56676de2fea7e8cdeb673f38
Vladdracoola <vladislav_pv@mail.ru>
2024-07-09 16:44:04 +0700

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,22 @@
+journal={}
+i = 0
+for i in range(99):
+ student_name=input('Введите имя ученика: ',)
+ student1=list(input('Введите оценки слитно:'))
+ quantity=int(len(student1))
+ print('Ученик' ,student_name)
+ marks=(student1)
+ marks=[int(item) for item in marks]
+ print('Оценки:', marks)
+ average=(sum(marks)/quantity)
+ average=round(average,2)
+ journal.update({student_name: average})
+ print('Средний балл: ',average)
+ if input('Продолжить?[да/нет] ') == 'нет':
+     break
+ else:
+     continue
+print(journal)
+
+
+
