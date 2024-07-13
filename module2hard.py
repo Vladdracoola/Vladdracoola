9783f66c85d83b65526cb771d2c22e34d94a146a
Vladdracoola <vladislav_pv@mail.ru>
2024-07-13 13:39:39 +0700

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -4,7 +4,7 @@ list_ = []
 for i in range(1, number + 1):
     for j in range(1, number + 1):
         summary_1 = i + j
-        if number % summary_1 == 0 and summary_1 > 2 and i != j and j > i:
+        if number % summary_1 == 0 and j > i:
             list_.append(i)
             list_.append(j)
 print('Число', number,'Шифр:', *list_)
