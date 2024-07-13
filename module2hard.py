4c62f3a89a55664f4c4bc1c71c0232f47c7ef07d
Vladdracoola <vladislav_pv@mail.ru>
2024-07-13 13:24:42 +0700

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(3, 20)
+list_ = []
+for i in range(1, number + 1):
+    for j in range(1, number + 1):
+        summary_1 = i + j
+        if number % summary_1 == 0 and summary_1 > 2 and i != j and j > i:
+            list_.append(i)
+            list_.append(j)
+print('Число', number,'Шифр:', *list_)
+
+
