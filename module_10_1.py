59e4ca4869b738661d6d342ad53524f0ccf145d5
Vladdracoola <vladislav_pv@mail.ru>
2024-08-26 17:49:32 +0700

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,44 @@
+import time
+from time import sleep
+from threading import Thread
+
+
+def write_words(word_count, file_name):
+    start_time = time.time()
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for word in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {word}" + '\n')
+            sleep(0.1)
+    end_time = time.time()
+    print(f"Завершилась запись в файл {file_name}. Время выполнения: {round(end_time - start_time, 2)} сек")
+
+
+global_start = time.time()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+global_end = time.time()
+print(f'Время записи всех файлов составило: {round(global_end - global_start, 2)} сек')
+
+stream_start = time.time()
+
+stream_1 = Thread(target=write_words, args=(10, 'example5.txt'))
+stream_2 = Thread(target=write_words, args=(30, 'example6.txt'))
+stream_3 = Thread(target=write_words, args=(200, 'example7.txt'))
+stream_4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+stream_1.start()
+stream_2.start()
+stream_3.start()
+stream_4.start()
+
+stream_1.join()
+stream_2.join()
+stream_3.join()
+stream_4.join()
+
+stream_end = time.time()
+print(f'Время потоковой записи всех файлов составило: {round(stream_end - stream_start, 2)} сек')
