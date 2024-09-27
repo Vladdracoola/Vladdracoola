c7eab4955e0be148445b99c9bfe7a0a93205a9cf
Vladdracoola <vladislav_pv@mail.ru>
2024-09-27 13:09:51 +0700

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)       
+''')
+
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+for i in range(10):
+    cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i + 1}", f"example{i + 1}@gmail.com", f"{(i + 1) * 10}", "1000"))
+
+cursor.execute(" UPDATE Users SET balance = ? WHERE id % 2 != 0", (500,))
+
+cursor.execute(" DELETE FROM Users WHERE (id + 2) % 3 = 0")
+
+cursor.execute(" SELECT username, email, age, balance FROM Users WHERE age != 60")
+
+users = cursor.fetchall()
+for user in users:
+    print(user)
+connection.commit()
+connection.close()
