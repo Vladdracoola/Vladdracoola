59b66270e4e85186a440a033e288fce842b129de
Vladdracoola <vladislav_pv@mail.ru>
2024-11-11 20:08:50 +0700

    module_17_1

---
+++ module_17/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import task, user
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return {'message': 'Welcome to Taskmanager'}
+
+
+app.include_router(task.router)
+app.include_router(user.router)
---
+++ module_17/app/routers/task.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/task', tags=['task'])
+
+
+@router.get('/')
+async def get_all_tasks():
+    pass
+
+
+@router.get('/task_id')
+async def task_by_id():
+    pass
+
+
+@router.post('/create')
+async def create_task():
+    pass
+
+
+@router.put('/update')
+async def update_task():
+    pass
+
+
+@router.delete('/delete')
+async def delete_task():
+    pass
