1c9224c3b634922f947942b126e24dd88b0aea4a
Vladdracoola <vladislav_pv@mail.ru>
2024-11-17 19:58:01 +0700

    module_17_1

---
+++ module_17/app/models/user.py
@@ -0,0 +1,15 @@
+from ..backend.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship('Task', back_populates='user', cascade='all, delete')
